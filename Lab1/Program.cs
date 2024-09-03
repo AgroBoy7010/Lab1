cc0462a6e2697a7de1e4b86f9b823aa62f6fb39e
ваня тащер <agroboy7010@mail.ru>
2024-09-03 16:31:30 +0300

    bug fixes

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 var a = new Person("Вася", "Петрович", "Пупкин");
+var x = new Person[10];
+var y = x.OrderBy(person => person.Name).ToArray();
+Console.WriteLine(a);
 public record Person(string Name,string LastName,string Patronomic,float Weight, DateTime Birthday)
 {
     public int Age=> DateTime.Now.Year-Birthday.Year;
+    public static Person Parse(string s)
+    {
+        var  array = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    }
 }
