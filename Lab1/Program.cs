8bbd49a8046fb191b7efcbb6d2d3e7be990e92bf
Agroboy <Agroboy@WIN-Q8079CNA9HD>
2024-09-03 16:21:44 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var a = new Person("Вася", "Петрович", "Пупкин");
+public record Person(string Name,string LastName,string Patronomic,float Weight, DateTime Birthday)
+{
+    public int Age=> DateTime.Now.Year-Birthday.Year;
+}
